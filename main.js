bbc155eff05b211b7c99f4ce52561295c18088b0
Nora_ccc <k82811@gmail.com>
2025-08-19 17:48:39 +0800

    新增元件、圓角map

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log('Hello world');
+
+
+const scrollBtn = document.querySelector('.scroll-top-btn');
+
+// 監聽點擊事件
+scrollBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'  
+    });
+});
